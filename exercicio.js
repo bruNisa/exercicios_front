6058d97e2caddedebd8a745c76623e85b1014102
Bruna Nisa <bnisa.dev@gmail.com>
2023-12-10 07:40:07 -0300

    exercicio_modulo26

---
+++ exercicio.js
@@ -0,0 +1,7 @@
+"use strict";
+const multiplica = (num1, num2) => num1 * num2;
+function saudacao(nome) {
+    return 'olá ' + nome;
+}
+console.log(multiplica(5, 4));
+console.log(saudacao('Bruna'));
